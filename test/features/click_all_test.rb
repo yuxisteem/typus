30610b7637f8e285acbc372e1f7d74fe1b0e1c82
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-11-13 12:06:35 +0100

    Quick and dirty way to verify urls.

---
+++ test/features/click_all_test.rb
@@ -0,0 +1,44 @@
+require 'feature_test_helper'
+
+class ClickAllTest < Capybara::Rails::TestCase
+
+  fixtures :typus_users
+
+  before do
+    Typus.stubs(:authentication).returns(:none) # auth is tested elsewhere
+  end
+
+  test 'all sections' do
+    urls.each do |url|
+      visit "/admin/#{url}"
+    end
+  end
+
+  def urls
+    %w(
+      dashboard
+      entries
+      projects
+      users
+      assets
+      cases
+      comments
+      pages
+      posts
+      article/entries
+      entry_defaults
+      categories
+      entry_bulks
+      entry_trashes
+      admin_users
+      typus_users
+      devise_users
+      birds
+      dogs
+      image_holders
+      invoices
+      orders
+    )
+  end
+
+end
