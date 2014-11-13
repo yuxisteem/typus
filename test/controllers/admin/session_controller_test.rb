503d144a7c5b9b085a07a312c464fbb7aa5de12c
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-11-13 16:58:00 +0100

    Fixed tests after the translation fixes.

--- test/controllers/admin/session_controller_test.rb
+++ test/controllers/admin/session_controller_test.rb
@@ -46,8 +46,8 @@ class Admin::SessionControllerTest < ActionController::TestCase
     assert_response :success
 
     # render new and verify title and header
-    assert_select 'title', 'Sign in &middot; Typus'
-    assert_select 'h2', 'Typus'
+    assert_select 'title', 'Please sign in &middot; Typus'
+    assert_select 'h2', 'Please sign in'
 
     # render session layout
     assert_template 'new'
