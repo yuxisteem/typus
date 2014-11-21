9fde074c5df828c734e24d01f19ff5b81a7d8b00
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-11-21 22:48:32 +0100

    Fixed a lot of images stuff.

--- app/helpers/admin/resources/data_types/paperclip_helper.rb
+++ app/helpers/admin/resources/data_types/paperclip_helper.rb
@@ -1,8 +1,11 @@
 module Admin::Resources::DataTypes::PaperclipHelper
 
+  def display_paperclip(item, attribute)
+    typus_paperclip_preview(item, attribute, 'paperclip_form_preview')
+  end
+
   def table_paperclip_field(attribute, item)
-    options = { width: 25 }
-    typus_paperclip_preview(item, attribute, options)
+    typus_paperclip_preview(item, attribute)
   end
 
   def link_to_detach_attribute_for_paperclip(attribute)
@@ -11,9 +14,9 @@ module Admin::Resources::DataTypes::PaperclipHelper
 
     if attachment.present? && !validators.include?(attribute) && attachment
       attribute_i18n = @item.class.human_attribute_name(attribute)
-      link = link_to( 
-        t('typus.buttons.remove'), 
-        { action: 'update', id: @item.id, _nullify: attribute, _continue: true }, 
+      link = link_to(
+        t('typus.buttons.remove'),
+        { action: 'update', id: @item.id, _nullify: attribute, _continue: true },
         { data: { confirm: t('typus.shared.confirm_question') } }
       )
 
@@ -25,17 +28,21 @@ module Admin::Resources::DataTypes::PaperclipHelper
     end
   end
 
-  def typus_paperclip_preview(item, attachment, options = {})
-    if (data = item.send(attachment)).exists?
-      styles = data.styles.keys
-      if data.content_type =~ /^image\/.+/ && styles.include?(Typus.file_preview) && styles.include?(Typus.file_thumbnail)
-        render 'admin/templates/paperclip_preview',
-               preview: data.url(Typus.file_preview, false),
-               thumb: data.url(Typus.file_thumbnail, false),
-               options: options
-      else
-        link_to data.original_filename, data.url(:original, false)
-      end
+  def typus_paperclip_preview(item, attribute, partial = 'paperclip_preview')
+    data = item.send(attribute)
+    return unless data.exists?
+
+    styles = data.styles.keys
+    if data.content_type =~ /^image\/.+/ && styles.include?(Typus.file_preview) && styles.include?(Typus.file_thumbnail)
+      render "admin/templates/#{partial}",
+             attachment: data,
+             preview: data.url(Typus.file_preview),
+             table_thumb: data.url(Typus.file_thumbnail),
+             thumb: data.url(Typus.file_thumbnail),
+             item: item,
+             attribute: attribute
+    else
+      link_to data.original_filename, data.url(:original, false)
     end
   end
 
