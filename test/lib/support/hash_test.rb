f0aa73466794d9c04962fdffdd08b95ca200807a
Francesc Esplugas <francesc.esplugas@gmail.com>
2014-12-18 22:16:45 +0100

    Hash#cleanup test updated

--- test/lib/support/hash_test.rb
+++ test/lib/support/hash_test.rb
@@ -3,15 +3,12 @@ require "test_helper"
 class HashTest < ActiveSupport::TestCase
 
   test "cleanup" do
-    whitelist = %w(controller action id _input _popup resource attribute)
-    whitelist.each do |w|
+    %w(controller action id _input _popup resource attribute).each do |w|
       expected = { w => w }
       assert_equal expected, expected.dup.cleanup
     end
-  end
 
-  test "cleanup rejects unwanted stuff" do
-    hash = {"_nullify" => "dragonfly"}
+    hash = {_nullify: 'dragonfly'}
     assert hash.cleanup.empty?
   end
 
