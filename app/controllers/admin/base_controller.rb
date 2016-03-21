8791933a08b5fca801898649fe7aa15e99436f1f
Francesc Esplugas <francesc.esplugas@gmail.com>
2016-03-21 10:50:57 +0100

    Added protect_from_forgery to Admin::BaseController

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,9 @@
 class Admin::BaseController < ActionController::Base
 
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
   include Admin::Hooks
   include Typus::Authentication::const_get(Typus.authentication.to_s.classify)
 
