0bbc0b64f96969c4452fe811ed284733102931bb
Francesc Esplugas <francesc.esplugas@gmail.com>
2016-07-27 20:46:40 +0200

    Fixed deprecation warnings.

--- test/controllers/admin/account_controller_test.rb
+++ test/controllers/admin/account_controller_test.rb
@@ -30,7 +30,7 @@ class Admin::AccountControllerTest < ActionController::TestCase
 
   test 'send_password redirects to new when no users' do
     TypusUser.delete_all
-    post :send_password, typus_user: { email: 'john@locke.com' }
+    post :send_password, params: { typus_user: { email: 'john@locke.com' } }
     assert_response :redirect
     assert_redirected_to new_admin_account_path
   end
@@ -38,7 +38,7 @@ class Admin::AccountControllerTest < ActionController::TestCase
   test 'create with invalid emails redirects to new' do
     TypusUser.delete_all
 
-    post :create, typus_user: { email: 'example.com' }
+    post :create, params: { typus_user: { email: 'example.com' } }
 
     assert_response :redirect
     assert_redirected_to action: :new
@@ -49,7 +49,7 @@ class Admin::AccountControllerTest < ActionController::TestCase
     TypusUser.delete_all
 
     assert_difference('TypusUser.count') do
-      post :create, typus_user: { email: 'john@example.com' }
+      post :create, params: { typus_user: { email: 'john@example.com' } }
     end
 
     assert_response :redirect
@@ -69,7 +69,7 @@ class Admin::AccountControllerTest < ActionController::TestCase
   end
 
   test 'send_password for unexisting email returns to send_password' do
-    post :send_password, typus_user: { email: 'unexisting' }
+    post :send_password, params: { typus_user: { email: 'unexisting' } }
     assert_response :success
     assert flash.empty?
   end
@@ -77,7 +77,7 @@ class Admin::AccountControllerTest < ActionController::TestCase
   test 'send_password for existing email' do
     Typus.stub(:mailer_sender, 'typus@example.com') do
       typus_user = typus_users(:admin)
-      post :send_password, typus_user: { email: typus_user.email }
+      post :send_password, params: { typus_user: { email: typus_user.email } }
       assert_response :redirect
       assert_redirected_to new_admin_session_path
       assert_equal 'Password recovery link sent to your email.', flash[:notice]
@@ -86,21 +86,21 @@ class Admin::AccountControllerTest < ActionController::TestCase
 
   test 'show with token generates a session a redirects user to edit' do
     typus_user = typus_users(:admin)
-    get :show, id: typus_user.token
+    get :show, params: { id: typus_user.token }
     assert_response :redirect
     assert_redirected_to controller: '/admin/typus_users', action: 'edit', id: typus_user.id
   end
 
   test 'show with token and return_to redirects to user dashboard' do
     typus_user = typus_users(:admin)
-    get :show, id: typus_user.token, return_to: '/admin'
+    get :show, params: { id: typus_user.token, return_to: '/admin' }
     assert_response :redirect
     assert_redirected_to 'http://test.host/admin'
   end
 
   test 'show with invalid token raises a 404 error' do
     assert_raises ActiveRecord::RecordNotFound do
-      get :show, id: 'unexisting'
+      get :show, params: { id: 'unexisting' }
     end
   end
 
